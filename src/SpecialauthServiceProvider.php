b5cdae2454990065023044535b80f0fa261d7c4b
Sebastien L'haire <sebastien@lhaire.org>
2023-03-07 19:53:07 +0100

    lang path

--- src/SpecialauthServiceProvider.php
+++ src/SpecialauthServiceProvider.php
@@ -16,7 +16,7 @@ class SpecialauthServiceProvider extends ServiceProvider{
    $this->publishes([
       __DIR__ . '/../config/specialauth.php' => config_path('specialauth.php'),
       __DIR__.'/../resources/views' => resource_path('views/vendor/specialauth'),
-      __DIR__.'/../lang' => app()->langPath(),
+      __DIR__.'/../lang' => app()->langPath('vendor/specialauth'),
       __DIR__ . '/../database/seeds/UsersTableSeeder.php' => database_path('seeds/UsersTableSeeder.php'),
    ]);
    $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
